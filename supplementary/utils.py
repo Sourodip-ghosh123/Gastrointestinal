01718351fd5a11a2a0d8749d3576248ee6caecd8
Sourodip Ghosh <51257384+Sourodip-ghosh123@users.noreply.github.com>
2021-04-14 15:12:33 +0530

    Update utils.py

--- supplementary/utils.py
+++ supplementary/utils.py
@@ -4,3 +4,8 @@ batch_size = 64
 epochs = 30
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
+
+final_preds = (vgg_outcomes.drop('image_id', axis=1)*0.33 + densenet_outcomes.drop('image_id', axis=1)*0.33 + cnn_outcomes.drop('image_id', axis=1)*0.33).to_numpy()
+final_preds = softmax(final_preds).argmax(1)
+
+accuracy_score(final_preds, df['label'].values)
